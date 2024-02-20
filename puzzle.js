2d98e9d895c416825eaf19be36d6f1cfd4fc7378
punit099 <113853214+punit099@users.noreply.github.com>
2024-02-20 14:04:41 +0530

    Add files via upload

---
+++ puzzle.js
@@ -0,0 +1,81 @@
+
+var hori = 3;
+var vert = 3;
+
+var currTile;
+var otherTile; //blank tile
+
+var turns = 0;
+
+// var imgOrder = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];   correct order
+var imgOrder = ["4", "2", "8", "5", "1", "6", "7", "9", "3"];
+
+window.onload = function() {
+    for (let r=0; r < hori; r++) {
+        for (let c=0; c < vert; c++) {
+
+            //<img id="0-0" src="1.jpg">
+            let tile = document.createElement("img");
+            tile.id = r.toString() + "-" + c.toString();
+            tile.src = imgOrder.shift() + ".jpg";
+
+            //DRAG FUNCTIONALITY
+            tile.addEventListener("dragstart", dragStart);  //click an image to drag
+            tile.addEventListener("dragover", dragOver);    //moving image around while clicked
+            tile.addEventListener("drop", dragDrop);        //drag an image over another image, drop the image
+            tile.addEventListener("dragend", dragEnd);      //after drag drop, swap the two tiles
+
+            document.getElementById("board").append(tile);
+
+        }
+    }
+}
+
+function dragStart() {
+    currTile = this; //this refers to the img tile being dragged
+}
+
+function dragOver(e) {
+    e.preventDefault();
+}
+
+
+
+function dragDrop() {
+    otherTile = this; //this refers to the img tile being dropped on
+}
+
+function dragEnd() {
+    if (!otherTile.src.includes("3.jpg")) {
+        return;
+    }
+
+    let currCoords = currTile.id.split("-"); //ex) "0-0" -> ["0", "0"]
+    let r = parseInt(currCoords[0]);
+    let c = parseInt(currCoords[1]);
+
+    let otherCoords = otherTile.id.split("-");
+    let r2 = parseInt(otherCoords[0]);
+    let c2 = parseInt(otherCoords[1]);
+
+    let Left = r == r2 && c2 == c-1;
+    let Right = r == r2 && c2 == c+1;
+
+    let Up = c == c2 && r2 == r-1;
+    let Down = c == c2 && r2 == r+1;
+
+    let isAdjacent = Left || Right || Up || Down;
+
+    if (isAdjacent) {
+        let currImg = currTile.src;
+        let otherImg = otherTile.src;
+
+        currTile.src = otherImg;
+        otherTile.src = currImg;
+
+        turns += 1;
+        document.getElementById("turns").innerText = turns;
+    }
+
+
+}
